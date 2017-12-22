b384507cd03989fdbbeacd3b6b4478c75c9958b2
Jay <jmtpenaflorida@gmail.com>
2017-12-22 12:46:48 +0800

    Refactored DI code

--- TestPluginApp.C/TestPluginApp.C/Form1.cs
+++ TestPluginApp.C/TestPluginApp.C/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Configuration;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -32,40 +27,6 @@ namespace TestPluginApp.C
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             PluginLogic.Input(textBox1.Text);
-        }
-    }
-
-    public class PluginsLogicDictionary
-    {
-        public Dictionary<string, string> PluginsLogic { get; set; }
-
-        public PluginsLogicDictionary(string section)
-        {
-            PluginsLogic = new Dictionary<string, string>();
-
-            var pluginsLogic = ConfigurationManager.GetSection(section) as NameValueCollection;
-            if (pluginsLogic != null)
-            {
-                foreach (string pluginsLogicKey in pluginsLogic.AllKeys)
-                {
-                    PluginsLogic.Add(pluginsLogicKey, pluginsLogic[pluginsLogicKey]);
-                }
-            }
-        }
-    }
-
-    public class PluginsLogicFactory
-    {
-        public T CreateInstance<T>(PluginsLogicDictionary pluginsLogicDictionary)
-        {
-            Type type = Type.GetType(((from pluginLogic in pluginsLogicDictionary.PluginsLogic
-                                       where pluginLogic.Key == typeof(T).Name
-                                       select pluginLogic).First().Value));
-
-            if (type.GetInterface(typeof(T).Name) == null)
-                throw new Exception("Not implementing valid interface");
-
-            return (dynamic) Activator.CreateInstance(type);
         }
     }
 }
